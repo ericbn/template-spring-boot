1cccb70348916ce8c4968fb7aa295891702152e4
Eric Nielsen <eric@amalgamar.com.br>
2015-09-30 14:03:46 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Application Template - Spring-Boot
+
+## Usage
+
+* `mvn test` to run the tests
+* `mvn spring-boot:run` to start the application
+
+## REST API
+
+### GET /
+
+#### Description
+
+Returns the "Hello World" string.
+
+#### Responses
+
+* 200: The string
+
+  ```
+Hello World
+  ```
---
+++ src/test/java/com/avenuecode/template/TemplateApplicationTest.java
@@ -0,0 +1,38 @@
+package com.avenuecode.template;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.AnnotationConfigWebContextLoader;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ContextConfiguration(classes = TemplateApplication.class, loader = AnnotationConfigWebContextLoader.class)
+public class TemplateApplicationTest {
+    @Autowired
+    private WebApplicationContext wac;
+    private MockMvc mockMvc;
+
+    @Before
+    public void setUp() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
+    }
+
+    @Test
+    public void testTemplate() throws Exception {
+        mockMvc.perform(
+                get("/")).
+            andExpect(status().isOk()).
+            andExpect(content().string("Hello World"));
+    }
+}
